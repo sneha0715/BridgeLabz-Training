33b833adacea2274494e19231700efe8a12813db
sneha0715 <157895540+sneha0715@users.noreply.github.com>
2025-12-17 15:42:36 +0530

    [Sneha Agrawal] classroom problem

---
+++ core-java-pratice/gcr-codebase/programming-elements/AreaOfCircle.java
@@ -0,0 +1,7 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double radius = 7.0;
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of the circle with radius " + radius + " is: " + area);
+    }
+}
